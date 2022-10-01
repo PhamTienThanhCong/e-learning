8af020cb7cbe0d8f3ba24578a3240e4acf82a8e4
congphamtienthanh <congphamtienthanh@gmail.com>
2022-10-01 22:15:35 +0700

    fixx

--- routes/web.php
+++ routes/web.php
@@ -90,34 +90,3 @@ Route::group([
 
 });
 // admin and seller
-
-// User
-Route::get('/dang-nhap', [userController::class, 'login'])->name('user.login');
-Route::post('/dang-nhap/xu-ly-dang-ky', [userController::class, 'register'])->name('user.register');
-Route::post('/dang-nhap/xu-ly-dang-nhap', [userController::class, 'loginProcessing'])->name('user.loginProcessing');
-Route::get('/tai-khoan-cua-toi/dang-xuat', [userController::class, 'logout'])->name('user.logout');
-
-Route::group([
-    'middleware' => UserWasLogin::class,
-],function(){
-    Route::get('/tai-khoan-cua-toi', [userController::class, 'myAccount'])->name('user.myAccount');
-    Route::get('/khoa-hoc-cua-toi', [homeViewController::class , 'myCourse'])->name('home.myCourse');
-    Route::post('/khoa-hoc/mua-khoa-hoc', [homeViewController::class, 'buyCourse'])->name('home.buyCourse');
-    Route::post('/khoa-hoc/ma-{course_id}/danh-gia', [homeViewController::class, 'ratingCourse'])->name('home.ratingCourse');
-    Route::get('/khoa-hoc/xem-khoa-{course_id}/bai-{lesson_id}', [homeViewController::class, 'learnCourse'])->name('home.learnCourse');
-    Route::get('/khoa-hoc/xem-khoa-{course_id}/bai-{lesson_id}/bai-ke-tiep', [homeViewController::class, 'next_lesson'])->name('home.nextLesson');
-    Route::post('/khoa-hoc/xem-khoa-{course_id}/bai-{lesson_id}/kiem-tra-cau-tra-loi', [homeViewController::class, 'check_answer'])->name('home.checkAnswer');
-    Route::get('/khoa-hoc/xem-khoa-{course_id}/hoan-thanh', [homeViewController::class, 'done_course'])->name('home.doneCourse');
-});
-
-Route::get('/khoa-hoc', [homeViewController::class , 'course'])->name('home.course');
-
-Route::get('/khoa-hoc/ma-{course_id}', [homeViewController::class , 'viewCourse'])->name('home.viewCourse');
-Route::get('/khoa-hoc/ma-{course_id}/dat-hang', [homeViewController::class , 'orderCourse'])->name('home.orderCourse');
-Route::get('/khoa-hoc/ma-{course_id}/huy-dat-hang', [homeViewController::class , 'unOrderCourse'])->name('home.unOrderCourse');
-
-Route::get('/gio-hang', [homeViewController::class , 'myCart'])->name('home.myCart');
-
-Route::get('/', [homeViewController::class , 'course'])->name('home.course');
-
-// User
